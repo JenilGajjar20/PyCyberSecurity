f4088b1abba4cd137c029b7e3c3f8dc32bc91774
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-06-07 12:56:53 +0530

    random password generator

---
+++ password_generator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+
+def generatePass(length):
+    # Defining the characters
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generating a password by randomly selecting characters
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+
+def checkLength(length):
+    # Check if the length is between 6 and 20
+    if length < 6 or length >= 20:
+        print("Password length must be between 6 and 16 characters")
+    else:
+        generated_pass = generatePass(length)
+        print("Generated password: ", generated_pass)
+
+
+if __name__ == '__main__':
+    # Enter length of the password
+    pass_length = int(input("Enter length of the password: "))
+
+    # Function to check the length of the password
+    checkLength(pass_length)
