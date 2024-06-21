308557e7db62d412875fa4bec5e20a7ab8a4b5b1
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-06-21 12:28:59 +0530

    comments added

--- socket/client.py
+++ socket/client.py
@@ -1,10 +1,16 @@
 import socket
 
+# Creating a socket object
 c = socket.socket()
 
+# Connecting the socket object to the specified address and port
 c.connect(('localhost', 9999))
 
+# Prompt the user to enter name
 name = input('Enter your name: ')
+
+# Sending the entered name to the server
 c.send(bytes(name, 'utf-8'))
 
+# Receiving the message from the server
 print(c.recv(1024).decode())
--- socket/server.py
+++ socket/server.py
@@ -1,20 +1,29 @@
 import socket
 
+# Creating a socket object
 s = socket.socket()
 print('Socket Created')
 
+# Binding the socket to a specific address and port number
 s.bind(('localhost', 9999))
 
+# Enable the socket to accept connections
 s.listen(3)
 print('Waiting for connection...')
 
 while True:
+    # Accept the connection from a client
+    # Created a new socket object (c) of the client
+    # addr is the address of the client
     c, addr = s.accept()
 
+    # receive the data from the client, with a buffer size of 1024 bytes
     name = c.recv(1024).decode()
 
     print("Connected with: ", addr, name)
 
-    c.send(bytes('Message', 'utf-8'))
+    # Sending a message to the client
+    c.send(bytes('Message to client', 'utf-8'))
 
+    # Closing the connection
     c.close()
